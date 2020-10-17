9dac250f9040a961ec4df30d52d8c4542fb5a793
Vsevolod <glasskitee@gmail.com>
2020-10-17 05:12:52 +0300

    Добавление нового фото 1.0

--- js/main.js
+++ js/main.js
@@ -78,3 +78,112 @@ for (let i = 0; i < cards.length; i++) {
 
 const pictureList = document.querySelector(`.pictures`);
 pictureList.appendChild(fragment);
+
+//Окно редактирования изображения
+
+const uploadFile = document.querySelector(`#upload-file`);
+const openForm = document.querySelector(`.img-upload__overlay`);
+const closeForm = openForm.querySelector(`.img-upload__cancel`);
+
+uploadFile.addEventListener(`change`, function () {
+  openForm.classList.remove(`hidden`);
+});
+
+const onPopupEscPress = function (evt) {
+  if (evt.key === `Escape`) {
+    closePopup();
+  }
+};
+
+const closePopup = function () {
+  openForm.classList.add(`hidden`);
+  document.addEventListener(`keydown`, onPopupEscPress);
+  uploadFile.value = ``;
+};
+
+closeForm.addEventListener(`click`, function () {
+  closePopup();
+});
+
+//Наложение эффекта на фото
+
+const photoPreview = document.querySelector(`.img-upload__preview`);
+const photoEffectList = document.querySelector(`.img-upload__effects`);
+const effectPin = document.querySelector(`.effect-level__pin`);
+const effectFilter = document.querySelector(`.img-upload__preview`);
+const filterWidth = document.querySelector(`.effect-level__line`);
+const filterCheckWidth = document.querySelector(`.effect-level__depth`);
+const filterType = ``;
+const filterUnits = ``;
+const filterMinValue = 0;
+const filterMaxValue = 0;
+const presentFilterValue = ``;
+
+const oldPhotoEffect = ``;
+
+const addPhotoEffect = function (evt) {
+  const currentPhotoEffect = `effects__preview--` + evt.target.value;
+  photoPreview.classList.add(currentPhotoEffect);
+  if (oldPhotoEffect !== ``) {
+    photoPreview.classList.remove(oldPhotoEffect);
+  }
+  oldPhotoEffect = currentPhotoEffect;
+
+  switch (evt.target.value) {
+    case `none`:
+      filterType = ``;
+      filterUnits = ``;
+      filterMinValue = 0;
+      filterMaxValue = 0;
+      break;
+    case `chrome`:
+      filterType = `grayscale`;
+      filterUnits = ``;
+      filterMinValue = 0;
+      filterMaxValue = 1;
+      break;
+    case `sepia`:
+      filterType = `sepia`;
+      filterUnits = ``;
+      filterMinValue = 0;
+      filterMaxValue = 1;
+      break;
+    case `marvin`:
+      filterType = `invert`;
+      filterUnits = `%`;
+      filterMinValue = 0;
+      filterMaxValue = 100;
+      break;
+    case `phobos`:
+      filterType = `blur`;
+      filterUnits = `px`;
+      filterMinValue = 0;
+      filterMaxValue = 3;
+      break;
+    case `heat`:
+      filterType = `brightness`;
+      filterUnits = ``;
+      filterMinValue = 1;
+      filterMaxValue = 3;
+      break;
+  }
+
+  presentFilterValue = (evt.target.value === `none`) ? effectFilter.style.filter = `` :
+    effectFilter.style.filter = filterType + `(` + filterMaxValue + filterUnits + `)`;
+};
+
+photoEffectList.addEventListener(`change`, addPhotoEffect);
+
+effectPin.addEventListener(`mouseup`, function (evt) {
+  presentFilterValue = (evt.target.value === `none`) ? effectFilter.style.filter = `` :
+    effectFilter.style.filter = filterType + `(` + photoEffectProportion() + filterUnits + `)`;
+});
+
+const photoEffectProportion = function () {
+  const photoEffectAttitude = (filterMaxValue - filterMinValue) / (filterWidth.clientWidth / filterCheckWidth.clientWidth) + filterMinValue;
+  return photoEffectAttitude;
+};
+
+//Валидация хештегов
+
+
