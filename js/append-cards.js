477688ebbc10e384c7d91967ec4091615a53d648
Vsevolod <glasskitee@gmail.com>
2020-11-01 18:14:18 +0300

    Получение данных с сервера 1.0

--- js/append-cards.js
+++ js/append-cards.js
@@ -1,18 +1,20 @@
 'use strict';
 
 (function () {
-  const pictureTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
-  const fragment = new DocumentFragment();
-  const cards = window.gallery.generateCards();
+  const loadCards = function (cards) {
+    const pictureTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
+    let fragment = new DocumentFragment();
 
-  for (let i = 0; i < cards.length; i++) {
-    let copyPicture = pictureTemplate.cloneNode(true);
-    copyPicture.querySelector(`.picture__img`).src = cards[i].avatar;
-    copyPicture.querySelector(`.picture__likes`).textContent = cards[i].likes;
-    copyPicture.querySelector(`.picture__comments`).textContent = cards[i].comments.length;
-    fragment.appendChild(copyPicture);
-  }
+    for (let i = 0; i < cards.length; i++) {
+      let copyPicture = pictureTemplate.cloneNode(true);
+      copyPicture.querySelector(`.picture__img`).src = cards[i].url;
+      copyPicture.querySelector(`.picture__likes`).textContent = cards[i].likes;
+      copyPicture.querySelector(`.picture__comments`).textContent = cards[i].comments.length;
+      fragment.appendChild(copyPicture);
+    }
 
-  const pictureList = document.querySelector(`.pictures`);
-  pictureList.appendChild(fragment);
+    const pictureList = document.querySelector(`.pictures`);
+    pictureList.appendChild(fragment);
+  };
+  window.load(loadCards);
 }());
